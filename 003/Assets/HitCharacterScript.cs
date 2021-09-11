ce5deb54fdc6787f69ff7c5a5c7106457cc8ef1f
KokuryuKaruma <syota_mobile@yahoo.ne.jp>
2021-09-11 15:15:04 +0900

    ScoreSystem

---
+++ 003/Assets/HitCharacterScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitCharacterScript : MonoBehaviour
+{
+    GameObject scoreObject;
+
+    void Start()
+    {
+        scoreObject = GameObject.Find("ScoreSystem");
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "Enemy")
+        {
+            scoreObject.GetComponent<ScoreScripts>().AddScore();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 003/Assets/ScoreScripts.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScripts : MonoBehaviour
+{
+    int score = 0;
+    public Text scoreText;
+
+    public void AddScore()
+    {
+        score++;
+    }
+
+    void Update()
+    {
+        scoreText.text = "SCORE:" + score.ToString();
+    }
+}
